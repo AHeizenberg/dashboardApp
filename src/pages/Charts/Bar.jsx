7637d54128f337c1ea34920a3d11ff09f48913cd
ahad.hamirani <ahadameen@hotmail.com>
2022-06-26 23:30:14 -0600

    Bar App & Graph working

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -4,41 +4,48 @@ import {
   SeriesCollectionDirective,
   SeriesDirective,
   Inject,
-  DateTime,
   Legend,
-  SplineAreaSeries,
+  Category,
+  Tooltip,
+  ColumnSeries,
+  DataLabel,
 } from "@syncfusion/ej2-react-charts";
 
 import {
-  areaCustomSeries,
-  areaPrimaryXAxis,
-  areaPrimaryYAxis,
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
 } from "../../data/dummy";
-
-import { Header } from "../../components";
+import { ChartsHeader } from "../../components";
 import { useStateContext } from "../../contexts/ContextProvider";
 
 const Bar = () => {
   const { currentMode } = useStateContext();
 
   return (
-    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-bg rounded-3xl">
-      <Header category="Area" title="Inflation Rate in Percentage" />
-      <ChartComponent
-        id="area-chart"
-        height="420px"
-        primaryXAxis={areaPrimaryXAxis}
-        primaryYAxis={areaPrimaryYAxis}
-        chartArea={{ border: { width: 0 } }}
-        tooltip={{ enable: true }}
-      >
-        <Inject services={[SplineAreaSeries, DateTime, Legend]} />
-        <SeriesCollectionDirective>
-          {areaCustomSeries.map((item, index) => (
-            <SeriesDirective key={index} {...item} />
-          ))}
-        </SeriesCollectionDirective>
-      </ChartComponent>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartsHeader category="Bar" title="Olympic Medal Counts - RIO" />
+      <div className=" w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={barPrimaryXAxis}
+          primaryYAxis={barPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+          legendSettings={{ background: "white" }}
+        >
+          <Inject
+            services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]}
+          />
+          <SeriesCollectionDirective>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            {barCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
     </div>
   );
 };
