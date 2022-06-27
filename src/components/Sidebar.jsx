cfaa11414ce0fba0dea5f186479c28b87b811ec5
ahad.hamirani <ahadameen@hotmail.com>
2022-06-26 22:40:59 -0600

    Dynamic Theme color switching - done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,12 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const {
+    activeMenu,
+    setActiveMenu,
+    screenSize,
+    currentColor,
+  } = useStateContext();
 
   const handleCloseSideBar = () => {
     if (activeMenu && screenSize <= 900) {
@@ -17,10 +22,10 @@ const Sidebar = () => {
   };
 
   const activeLink =
-    "flex items-center gap-5 p1-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
 
   const normalLink =
-    "flex items-center gap-5 p1-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 hover:text-black hover:bg-light-gray m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 hover:text-black hover:bg-light-gray m-2";
 
   return (
     <div>
@@ -58,6 +63,9 @@ const Sidebar = () => {
                     <NavLink
                       to={`/${link.name}`}
                       key={link.name}
+                      style={({ isActive }) => ({
+                        backgroundColor: isActive ? currentColor : "",
+                      })}
                       onClick={handleCloseSideBar}
                       className={({ isActive }) =>
                         isActive ? activeLink : normalLink
--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -14,18 +14,20 @@ export const ContextProvider = ({ children }) => {
   const [isClicked, setIsClicked] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
   const [currentColor, setCurrentColor] = useState("#03C9D7");
-  const [currentMode, setCurrentMode] = useState("#Light");
+  const [currentMode, setCurrentMode] = useState("Light");
 
   const [themeSettings, setThemeSettings] = useState(false);
 
   const setMode = (e) => {
     setCurrentMode(e.target.value);
     localStorage.setItem("themeMode", e.target.value);
+    setThemeSettings(false);
   };
 
-  const setColor = (e) => {
-    setCurrentColor(e.target.value);
-    localStorage.setItem("colorMode", e.target.value);
+  const setColor = (color) => {
+    setCurrentColor(color);
+    localStorage.setItem("colorMode", color);
+    setThemeSettings(false);
   };
 
   const handleClick = (clicked) => {
@@ -44,8 +46,8 @@ export const ContextProvider = ({ children }) => {
         setScreenSize,
         currentColor,
         currentMode,
-        setCurrentColor,
-        setCurrentMode,
+        setColor,
+        setMode,
         themeSettings,
         setThemeSettings,
       }}
