b3b996d9074b1fe4c435c3af31f7b3f0817e55d5
ahad.hamirani <ahadameen@hotmail.com>
2022-06-26 23:23:43 -0600

    Fixed some dashboard bugs

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -5,40 +5,37 @@ import {
   SparklineTooltip,
 } from "@syncfusion/ej2-react-charts";
 
-const SparkLine = ({ currentColor, id, type, height, width, data, color }) => {
-  const tempData = [
-    { x: 1, yval: 2 },
-    { x: 2, yval: 6 },
-    { x: 3, yval: 8 },
-    { x: 4, yval: 5 },
-    { x: 5, yval: 10 },
-  ];
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
 
-  return (
-    // <SparklineComponent
-    //   id={id}
-    //   height={height}
-    //   width={width}
-    //   lineWidth={1}
-    //   valueType="Numeric"
-    //   fill={color}
-    //   border={{ color: currentColor, width: 2 }}
-    //   dataSource={data}
-    //   xName="x"
-    //   yName="y"
-    //   type={type}
-    //   tooltipSettings={{
-    //     visible: true,
-    //     format: "${x} : data ${y}",
-    //     trackLineSettings: {
-    //       visible: true,
-    //     },
-    //   }}
-    // >
-    //   <Inject services={[SparklineTooltip]} />
-    // </SparklineComponent>
-    <div></div>
-  );
-};
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        tooltipSettings={{
+          visible: true,
+          // eslint-disable-next-line no-template-curly-in-string
+          format: "${x} : data ${yval}",
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        markerSettings={{ visible: ["All"], size: 2.5, fill: currentColor }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
+  }
+}
 
 export default SparkLine;
